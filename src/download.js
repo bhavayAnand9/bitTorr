096d0d271e9ea20bb09d68a2372ff1ec32336e79
Bhavay Anand <bhavayanandcse@gmail.com>
2019-03-31 02:19:10 +0530

    oh man this project is storming my brain

--- src/download.js
+++ src/download.js
@@ -5,7 +5,67 @@ const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
 
 module.exports = torrent => {
-	tracker.getPeers(torrent, peers => {
-		
-	});
+  tracker.getPeers(torrent, peers => {
+    // 1
+    peers.forEach(peer => download(peer, torrent));
+  });
 };
+
+function download(peer, torrent) {
+  const socket = new net.Socket();
+  socket.on('error', console.log);
+  socket.connect(peer.port, peer.ip, () => {
+    // 1
+    socket.write(message.buildHandshake(torrent));
+  });
+  // 2
+  onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
+
+// 2
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
+}
+
+// 3
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1) === 'BitTorrent protocol';
+}
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
+}
+
+function chokeHandler() { ... }
+
+function unchokeHandler() { ... }
+
+function haveHandler(payload) { ... }
+
+function bitfieldHandler(payload) { ... }
+
+function pieceHandler(payload) { ... }
